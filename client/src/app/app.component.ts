e22a888c72eb45c42ab5a6cc58fcc3e8000d2760
Arie Blonk <arieblo@gmail.com>
2020-12-17 19:55:23 +0100

    added basket functionality api frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import {Component, OnInit} from '@angular/core';
+import {BasketService} from './basket/basket.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
-  }
+	constructor(private basketService: BasketService) {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	    const basketId = localStorage.getItem('basket_id');
+
+	    if (basketId) {
+	      this.basketService.getBasket(basketId).subscribe(() => {
+	        console.log('initialized basket');
+          }, error => {
+	        console.log(error);
+          })
+        }
+	}
 }
