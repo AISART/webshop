0111618bc6356f8cf0bd2469f632ed935f09bad7
Arie Blonk <arieblo@gmail.com>
2021-01-05 14:07:13 +0100

    added stepper with checkout module

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -5,6 +5,7 @@ import {BehaviorSubject} from 'rxjs';
 import {Basket, IBasket, IBasketItem, IBasketTotal} from '../shared/models/basket';
 import {map} from 'rxjs/operators';
 import {IProduct} from '../shared/models/product';
+import {IDeliveryMethod} from '../shared/models/deliveryMethods';
 
 @Injectable({
 	providedIn: 'root'
@@ -17,8 +18,14 @@ export class BasketService {
 
     private basketTotalSource = new BehaviorSubject<IBasketTotal>(null);
     basketTotal$ = this.basketTotalSource.asObservable();
+    shipping = 0;
 
 	constructor(private http: HttpClient) {
+	}
+
+	setShippingPrice(deliveryMethod: IDeliveryMethod) {
+		this.shipping = deliveryMethod.price;
+		this.calculateTotals();
 	}
 
 	getBasket(id: string) {
@@ -85,6 +92,12 @@ export class BasketService {
 		}
 	}
 
+	deleteLocalBasket(id: string) {
+		this.basketSource.next(null);
+		this.basketTotalSource.next(null);
+		localStorage.removeItem('basket_id');
+	}
+
 	deleteBasket(basket: IBasket) {
 		return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(() => {
 			this.basketSource.next(null);
@@ -129,7 +142,7 @@ export class BasketService {
 
 	private calculateTotals() {
 		const basket = this.getCurrentBasketValue();
-		const shipping = 0;
+		const shipping = this.shipping;
 		const subTotal = basket.items.reduce((a, b) =>(b.price * b.quantity) + a, 0);
 		const total = subTotal + shipping;
 
